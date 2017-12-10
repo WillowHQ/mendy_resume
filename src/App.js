95ca1de9f5f382e5f0838dd1f87eb137fb567880
Thom Lamb <fitnessevangelist@gmail.com>
2017-12-10 12:55:35 -0500

    Changes title of Mendy's app to Thom
    - Because he is cool.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Mendy's App</h1>
+          <h1 className="App-title">Thom's App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
